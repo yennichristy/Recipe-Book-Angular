9e8c99bcc2106c68d0c9a04d1e87a10f479aca73
Yenni Permatasari <yennikristianip@gmail.com>
2020-04-11 15:04:36 +0700

    [Yenni] - Rearrange components and connect it to module

---
+++ src/app/recipes/recipes.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'recipes',
+  templateUrl: './recipes.component.html',
+  styleUrls: ['./recipes.component.css'],
+})
+export class RecipesComponent {}
